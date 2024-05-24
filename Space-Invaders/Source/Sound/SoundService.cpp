a15f53e14225a0d6ca9d2a345f88ff7c7ee31f3e
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-25 05:02:04 +0530

    Create Sound Service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,19 @@
+#include "../../Header/Sound/SoundService.h"
+
+namespace Sound {
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+	}
+	void SoundService::loadSoundFromFile()
+	{
+	}
+	void SoundService::playSound(SoundType)
+	{
+	}
+	void SoundService::playBackroundMusic()
+	{
+	}
+	void SoundService::initialize()
+	{
+	}
+}
