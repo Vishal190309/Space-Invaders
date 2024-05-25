583194dddefde3254f07c9b7569cf58596a224e5
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-26 03:09:56 +0530

    Create Bullet MVC

---
+++ Space-Invaders/Header/Bullet/BulletController.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../Projectile/IProjectile.h"
+
+namespace Bullet {
+	enum class BulletType;
+	class BulletModel;
+	class BulletView;
+
+	class BulletController : public Projectile::IProjectile {
+	protected:
+
+		BulletModel* bulletModel;
+		BulletView* bulletView;
+
+		void updateProjectilePosition() override;
+
+		void moveUp();
+		void moveDown();
+		void handleOutOfBounds();
+	public:
+		BulletController(BulletType);
+		virtual ~BulletController();
+
+		void initialize(sf::Vector2f position, Bullet::MovementDirection direction) override;
+		void update() override;
+		void render() override;
+		sf::Vector2f getProjectilePosition() override;
+		BulletType getBulletType();
+	};
+}
