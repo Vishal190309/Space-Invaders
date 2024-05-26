123ff33b9f37abbf478b6f36e3a2ff975e8406be
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-05-26 06:04:57 +0530

    Spawn Bullets

---
+++ Space-Invaders/Source/Bullet/BulletService.cpp
@@ -0,0 +1,66 @@
+#include "../../Header/Bullet/BulletService.h"
+#include "../../Header/Bullet/BulletConfig.h"
+#include "../../Header/Bullet/BulletController.h"
+#include "../../Header/Bullet/Controllers/LaserBulletController.h"
+#include "../../Header/Bullet/Controllers/ForstBulletController.h"
+#include "../../Header/Bullet/Controllers/TorpedoeController.h"
+#include "../../Header/Projectile/IProjectile.h"
+
+
+namespace Bullet {
+	BulletController* BulletService::createBullet(BulletType type)
+	{
+		switch (type)
+		{
+		case Bullet::BulletType::LASER_BULLET:
+			return new Controllers::LaserBulletController(type);
+			break;
+		case Bullet::BulletType::TORPEDO:
+			new Controllers::TorpedoeBulletController(type);
+			break;
+		case Bullet::BulletType::FROST_BULLET:
+			new Controllers::ForstBulletController(type);
+			break;
+		}
+	}
+	void BulletService::destroy()
+	{
+		for (int i = 0; i < listOfProjectile.size(); i++) {
+			delete(listOfProjectile[i]);
+		}
+	}
+	BulletService::BulletService()
+	{
+	}
+	BulletService::~BulletService()
+	{
+		destroy();
+	}
+	void BulletService::initialize()
+	{
+	}
+	void BulletService::update()
+	{
+		for (int i = 0; i < listOfProjectile.size(); i++) {
+			listOfProjectile[i]->update();
+		}
+	}
+	void BulletService::render()
+	{
+		for (int i = 0; i < listOfProjectile.size(); i++) {
+			listOfProjectile[i]->render();
+		}
+	}
+	void BulletService::spawnBullet(BulletType type, sf::Vector2f position, MovementDirection direction)
+	{
+		BulletController* controller = createBullet(type);
+		controller->initialize(position, direction);
+		listOfProjectile.push_back(controller);
+	}
+	void BulletService::destroyBullet(BulletController* controller)
+	{
+		
+		listOfProjectile.erase(std::remove(listOfProjectile.begin(), listOfProjectile.end(), controller), listOfProjectile.end());
+		delete(controller);
+	}
+}
